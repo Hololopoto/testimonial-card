d5e5719be4ab847e67a03ea3cc8be5b269007d17
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-05-01 14:15:46 +0300

    Add index.js, style.css, and .gitignore files

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function index() {
+  return (
+    <div className="bg-">
+      <div className=""></div>
+    </div>
+  );
+}
+
+export default index;
